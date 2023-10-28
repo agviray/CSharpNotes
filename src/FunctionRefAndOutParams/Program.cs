59ca5c59367e487292a9e4bade9ef6be52e87c80
Allan Viray <allang.v.dev@gmail.com>
2023-10-28 17:44:29 -0500

    Add notes and demonstration of out keyword usage

--- src/FunctionRefAndOutParams/Program.cs
+++ src/FunctionRefAndOutParams/Program.cs
@@ -19,10 +19,32 @@ namespace FunctionRefAndOutParams {
       arg1+= 10;
       Console.WriteLine($"{arg1}");
     }
+
+    // ====================================================
+    // - About the "out" keyword
+    // ====================================================
+    // - Functions that use the "out" keyword with a param means 2 things:
+    //    1) The associated param returns a value.
+    //    2) The associated param is not used to supply data to the function.
+    // - Note that the out keyword must also be stated in the function call.
+    static void PrintUserBirthDate(string name, string month, int date, out string birthMonth, out int birthDate) {
+      birthMonth = month;
+      birthDate = date;
+      Console.WriteLine($"Hello there, {name}. Your birth date is: ");
+    }
+
     static void Main(string[] args) {
       int valForFuncA = 20;
       int valForFuncB = 30;
+      string name = "Allan";
+      string month = "October";
+      int date = 21;
+      string yourBirthMonth; 
+      int yourBirthDate;
 
+      // ------------------
+      // Calling functions
+      // ------------------
       // - FunctionA will log 30 as expected.
       // - However, the value of valForFuncA did not change. It still has
       //   the original value of 20.
@@ -36,9 +58,15 @@ namespace FunctionRefAndOutParams {
       Console.WriteLine("-------Testing FunctionB-------");
       Console.WriteLine("-------------------------------");
       Console.WriteLine($"FunctionB logs: ");
-      // - The "ref" keyword must be used in the function call as well.
+      // - The "ref" keyword must be used in the function call.
       FunctionB(ref valForFuncB);
       Console.WriteLine($"And the value of the original variable, valForFuncB, is also changed to: {valForFuncB}");
+      Console.WriteLine("----------------------------------------");
+      Console.WriteLine("-------Testing PrintUserBirthDate-------");
+      Console.WriteLine("----------------------------------------");
+      // - The "out" keyword must be used in the function call.
+      PrintUserBirthDate(name, month, date, out yourBirthMonth, out yourBirthDate);
+      Console.WriteLine($"{yourBirthMonth} {yourBirthDate}");
     }
   }
 }
